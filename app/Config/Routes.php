ac6b1dbb376c3d7ed0d8bbfb380f517bf8678af6
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-08-23 14:46:27 -0300

    template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,8 @@
 
 namespace Config;
 
+use App\Controllers\Super\HomeController;
+
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
@@ -30,6 +32,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('super', [HomeController::class, 'index'], ['as' => 'super.home']);
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Controllers/Super/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers\Super;
+
+use App\Controllers\BaseController;
+
+class HomeController extends BaseController
+{
+    public function index()
+    {
+
+        $data = [
+            'title' =>  'Home'
+        ];
+        return view('Back/Home/index', $data);
+    }
+}
